dcade7cd762f1a7e5c7b5d9d6be0bdac64b8887d
davfu <daveyblazefoo@gmail.com>
2023-06-29 01:43:08 -0700

    use smaller dictionary, other dictionary was too big and had words unrecognized by wordhunt game

--- filter_words.py
+++ filter_words.py
@@ -1,12 +1,12 @@
 import re
 
 # open and read input file
-with open('test.txt', 'r') as file: lines = file.readlines()
+with open('dictionary.txt', 'r') as file: lines = file.readlines()
 print("Original word count: " + str(len(lines)))
 # define chars not wanted in string
 pattern = r'[0-9!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~]'
 # filter out lines that match pattern
-filtered_lines = [line.lower() for line in lines if not re.search(pattern, line)]
+filtered_lines = [line.lower() for line in lines if not re.search(pattern, line) and len(line) > 3]
 # rewrite input file
-with open('test.txt', 'w') as file: file.writelines(filtered_lines)
+with open('dictionary.txt', 'w') as file: file.writelines(filtered_lines)
 print("Filtered word count: " + str(len(filtered_lines)))
