f66d1d7dc088ff09d805b26f52f6b95513daa27c
davfu <daveyblazefoo@gmail.com>
2023-06-30 22:08:06 -0700

    Merge branch 'main' of github.com:davfu/wordhuntsolver

--- WordGamesSolvers.py
+++ WordGamesSolvers.py
@@ -1,8 +1,8 @@
+import copy
 from Trie import Trie
 
 class WordGameSolver:
-    def __init__(self, board) -> None:
-        self.board = [char for char in board]
+    def __init__(self) -> None:
         self.words_set = set()
         self.words_map = {}
         self.filename = 'dictionary.txt'
@@ -12,7 +12,13 @@ class WordGameSolver:
         with open(self.filename, 'r') as file: dictionary_words = [word.strip() for word in file.readlines()]
         for word in dictionary_words:
             self.trie.insert(word)
-    
+            
+    # finds words in all nodes calling DFS method
+    def find_words(self):
+        seen = []
+        for node in range(len(self.board)):
+            self.dfs_words(node, seen)
+
     # classify the words on basis of word length
     def classify_words(self):
         for word in self.words_set:
@@ -33,13 +39,9 @@ class WordGameSolver:
 
 class Anagrams(WordGameSolver):
     def __init__(self, board) -> None:
-        super().__init__(board)
+        super().__init__()
+        self.board = [char for char in board]
         if not len(self.board) == 6: raise Exception("Board must be 6 characters.")
-    
-    def find_words(self):
-        seen = []
-        for node in range(len(self.board)):
-            self.dfs_words(node, seen)
         
     def dfs_words(self, node, seen):
         if node not in seen:
@@ -52,7 +54,8 @@ class Anagrams(WordGameSolver):
 
 class WordHunt(WordGameSolver):
     def __init__(self, board) -> None:
-        super().__init__(board)
+        super().__init__()
+        self.board = [char for char in board]
         if not len(self.board) == 16: raise Exception("Board must be 16 characters.")
 
     # returns nodes that can be neighbors
@@ -76,12 +79,6 @@ class WordHunt(WordGameSolver):
         operator = positions[config[row][col]]
         valid_neighbors = [node + val for val in operator if node + val not in seen]
         return valid_neighbors
-    
-    # finds words in all nodes calling DFS method
-    def find_words(self):
-        seen = []
-        for node in range(len(self.board)):
-            self.dfs_words(node, seen)
 
     # finds words in a given node using DFS
     def dfs_words(self, node, seen):
@@ -99,4 +96,54 @@ class WordHunt(WordGameSolver):
             if self.trie.valid_prefix(cur_word):
                 for node in neighbors: self.dfs_words(node, new_seen)
 
+class WordBites(WordGameSolver):
+    def __init__(self, singles, vert, hor) -> None:
+        super().__init__()
+        self.board = {'s': singles, 'v': vert, 'h': hor}
+    
+    def find_words(self):
+        remaining = {'s': copy.deepcopy(self.board['s']), 
+                     'v': copy.deepcopy(self.board['v']), 
+                     'h': copy.deepcopy(self.board['h'])}
+        for key in self.board:
+            for val in self.board[key]:
+                self.vert_words((key, val), remaining, '')
 
+    def vert_words(self, node, remaining, prev_word): 
+        if node[1] in remaining[node[0]]:
+            # remove node from remaining
+            new_remaining = {key: [value[i] for i in range(len(value)) if value[i] != node[1] or (value[i] == node[1] and i != value.index(node[1]))] if key == node[0] else value for key, value in remaining.items()}
+            if node[0] == 'h': iters = 2 # two possible words:
+            else: iters = 1
+            for i in range(iters):
+                if iters == 2: cur_word = prev_word + node[1][i]
+                else: cur_word = prev_word + node[1]
+                if len(cur_word) >= 3 and self.trie.valid_word(cur_word): self.words_set.add(cur_word)
+                if self.trie.valid_prefix(cur_word): 
+                    for key in new_remaining:
+                        for val in new_remaining[key]:
+                            self.vert_words((key, val), new_remaining, cur_word)
+
+            # if node[0] == 'h':
+            #     cur_word1, cur_word2 = prev_word + node[1][0], prev_word + node[1][1]
+            #     if len(cur_word1) >= 3 and self.trie.valid_word(cur_word1): self.words_set.add(cur_word1)
+            #     if len(cur_word2) >= 3 and self.trie.valid_word(cur_word2): self.words_set.add(cur_word2)
+            #     if self.trie.valid_prefix(cur_word1): 
+            #         for key in new_remaining:
+            #             for val in new_remaining[key]:
+            #                 self.vert_words((key, val), new_remaining, cur_word1)
+            #     if self.trie.valid_prefix(cur_word2):
+            #         for key in new_remaining:
+            #             for val in new_remaining[key]:
+            #                 self.vert_words((key, val), new_remaining, cur_word2)
+            # else:
+            #     cur_word = prev_word + node[1]
+            #     if len(cur_word) >= 3 and self.trie.valid_word(cur_word): self.words_set.add(cur_word)
+            #     if self.trie.valid_prefix(cur_word):
+            #         for key in new_remaining:
+            #             for val in new_remaining[key]:
+            #                 self.vert_words((key, val), new_remaining, cur_word)
+
+    def hor_words(self):
+        hor_starters = [char for string in self.board['v'] for char in string] + self.board['h'] + self.board['s']
+        print("horizontal starters" + str(hor_starters))
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from WordGamesSolvers import Anagrams, WordHunt
+from WordGamesSolvers import Anagrams, WordHunt, WordBites
 
-select = int(input("Anagrams [1] or WordHunt [2]: "))
+select = int(input("Anagrams [1] or WordHunt [2] or WordBites [3]: "))
 if  select == 1:
     board = input("Enter board: ")
     cur = Anagrams(board)
     try: cur.print_words()
     except Exception as e: print("Error:", str(e))
+
 elif select == 2:
     board = input("Enter board: ")
     cur = WordHunt(board)
     try: cur.print_words()
     except Exception as e: print("Error:", str(e))
+
+elif select == 3:
+    singles = input("Enter single tiles (differentiate tiles using a space): ").split()
+    vert = input("Enter vertical tiles: ").split()
+    hor = input("Enter horizontal tiles: ").split()
+    cur = WordBites(singles, vert, hor)
+    cur.print_words()
+    
 else:
     print("Input not recognized.")
